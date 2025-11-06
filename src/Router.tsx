d703bb25536f6b907cfb705e4a507d309098f6bc
bae080311 <s24024@gsm.hs.kr>
2025-11-06 14:43:02 +0900

    fix: build error

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router";
 import { Main, MyBooks, Onboarding } from "./pages";
 import { AppLayout } from "./layout";
+import Detail from "./pages/Detail";
 
 export const Router = createBrowserRouter([
   {
