517b69a255a9117bf136eb7667b056535a0d237f
bae080311 <s24024@gsm.hs.kr>
2025-11-06 15:53:35 +0900

    feat: button props

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 
-interface ButtonProps {
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   children: React.ReactNode;
 }
 
-export default function Button({ children }: ButtonProps) {
+export default function Button({ children, ...props }: ButtonProps) {
   return (
-    <button className="font-hsjandari text-white w-full py-3 text-2xl rounded-2xl bg-[#5eb5a8]">
+    <button
+      className="font-hsjandari text-white w-full py-3 text-2xl rounded-2xl bg-[#5eb5a8]"
+      {...props}
+    >
       {children}
     </button>
   );
