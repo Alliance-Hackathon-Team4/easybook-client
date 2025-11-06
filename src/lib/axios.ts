513197ef37037b4668c22709ed2bd18ebbc4aa9b
bae080311 <s24024@gsm.hs.kr>
2025-11-06 14:37:00 +0900

    chore: warning skip

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -2,6 +2,10 @@ import axios from "axios";
 
 const instance = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL,
+  withCredentials: true,
+  headers: {
+    "ngrok-skip-browser-warning": "true",
+  },
 });
 
 export default instance;
