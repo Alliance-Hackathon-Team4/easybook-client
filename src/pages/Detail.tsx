6c3c55fc3c0733756ca1f031e318c879cc2011ad
bae080311 <s24024@gsm.hs.kr>
2025-11-06 19:41:30 +0900

    fix: 경로

--- src/pages/Detail.tsx
+++ src/pages/Detail.tsx
@@ -10,7 +10,7 @@ export default function Detail() {
   const { data } = useGetBook(id);
 
   const handleClick = () => {
-    navigate("/read/" + id);
+    navigate("/read/" + id + "/" + data?.currentPage);
   };
   return (
     <div>
