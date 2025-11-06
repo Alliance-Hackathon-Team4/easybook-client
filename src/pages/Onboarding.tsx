8001bb92b3886464bf1bb82fc3295e0f6d16ec42
minsu0707 <mandoo4137@gmail.com>
2025-11-06 10:23:08 +0900

    feat : 온보딩 페이지

--- src/pages/Onboarding.tsx
+++ src/pages/Onboarding.tsx
@@ -1,3 +1,13 @@
+import { ezbook } from "../assets";
+
 export const Onboarding = () => {
-  return <></>
-}
+  return (
+    <div className="flex items-center justify-center h-screen bg-[#5eb5a8]">
+      <img
+        src={ezbook}
+        alt="이지북 로고"
+        className="w-64 h-auto drop-shadow-[2px_4px_2px_rgba(0,0,0,0.4)]"
+      />
+    </div>
+  );
+};
