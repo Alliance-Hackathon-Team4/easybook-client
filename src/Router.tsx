4d98e7cd793629124ff25af22ebf2faa57cc47f3
bae080311 <s24024@gsm.hs.kr>
2025-11-06 11:10:39 +0900

    feat: detail

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,9 +1,14 @@
 import { createBrowserRouter } from "react-router";
 import { Onboarding } from "./pages";
+import Detail from "./pages/Detail";
 
 export const Router = createBrowserRouter([
   {
     path: "/",
     element: <Onboarding />,
+  },
+  {
+    path: "/detail/:id",
+    element: <Detail />,
   },
 ]);
