83d833b6bc225a7a77817e767734dc2d45dfb568
minsu0707 <mandoo4137@gmail.com>
2025-11-07 11:47:54 +0900

    feat : 온보딩

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,24 @@
+import { useState, useEffect } from "react";
 import { RouterProvider } from "react-router";
 import { Router } from "./Router";
 import { QueryProvider } from "./lib/queryProvider";
+import { Onboarding } from "./pages";
 
 function App() {
+  const [showOnboarding, setShowOnboarding] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowOnboarding(false);
+    }, 1500);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  if (showOnboarding) {
+    return <Onboarding />;
+  }
+
   return (
     <>
       <QueryProvider>
